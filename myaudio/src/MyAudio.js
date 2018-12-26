f9d9a0eaf414f4c6e1b03f4336081dc1c9bafb75
MonkeyBean <monkeybean_zhang@163.com>
2018-12-26 22:45:48 +0800

    删除无用方法

--- myaudio/src/MyAudio.js
+++ myaudio/src/MyAudio.js
@@ -1,28 +1,3 @@
-/**
- * Created by WangChong on 16/5/10.
- * 创建的第三方库项目
- * 编写MyAudio
- * 编写myaudio.d.ts
- * declare function LoadAudio():boolean;
- * declare function PlayAudio(num : number, volume : number, loop : boolean, isContinue :boolean);
- * declare function PlayAllAudio();
- * declare function StopAudio(num : number);
- * declare function StopAllAudio();
- * build myaudio
- * build xypu3 -e
- */
-
-function SupportAudio()
-{
-    return audio.SupportAudio();
-}
-
-function LoadAudio()
-{
-    //console.log("isover"+audio.isover());
-    return audio.isover();
-}
-
 //第几个音乐,音量(大于0),是否循环,是否继续播放
 function PlayAudio (num, volume, isLoop, isContinue)
 {
@@ -31,12 +6,6 @@ function PlayAudio (num, volume, isLoop, isContinue)
         audio.play(num, volume, isLoop, isContinue);
     }
     //console.log("播放了" + num);
-}
-
-function PlayAllAudio()
-{
-    audio.playAll();
-    //console.log("播放了所有声音");
 }
 
 function StopAudio (num)
